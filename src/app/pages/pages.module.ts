344e9d1258c9bcccd782d23bf18817f809854f19
saurabh.palatkar <saurabh.palatkar@globallogic.com>
2019-04-26 15:03:45 +0530

    Removed unnecessary code

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
-
-import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
-import { PagesRoutingModule } from './pages-routing.module';
 import { ThemeModule } from '../@theme/theme.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { PagesRoutingModule } from './pages-routing.module';
+import { PagesComponent } from './pages.component';
 
-const PAGES_COMPONENTS = [
-  PagesComponent,
-];
+const PAGES_COMPONENTS = [PagesComponent];
 
 @NgModule({
-  imports: [
-    PagesRoutingModule,
-    ThemeModule,
-    DashboardModule,
-    ECommerceModule,
-    MiscellaneousModule,
-  ],
-  declarations: [
-    ...PAGES_COMPONENTS,
-  ],
+  imports: [PagesRoutingModule, ThemeModule, DashboardModule, MiscellaneousModule],
+  declarations: [...PAGES_COMPONENTS],
 })
-export class PagesModule {
-}
+export class PagesModule {}
