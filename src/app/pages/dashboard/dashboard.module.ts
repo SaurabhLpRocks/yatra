bbdf416536d6f2e31ba152c0fc39241880339bce
saurabh.palatkar <saurabh.palatkar@globallogic.com>
2019-04-26 15:12:19 +0530

    Added card in dashboard

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
+import { NbCardModule } from '@nebular/theme';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
 import { WeatherComponent } from './weather/weather.component';
 
-
-
-
 @NgModule({
-  imports: [ThemeModule, NgxEchartsModule],
+  imports: [ThemeModule, NgxEchartsModule, NbCardModule],
   declarations: [DashboardComponent, WeatherComponent],
 })
 export class DashboardModule {}
