ecc403a5d3ce1f59057c5cd3839e433ef4d01582
Swapnil <swapnilwasnik007@gmail.com>
2019-04-27 09:08:18 +0530

    Added python api in project

---
+++ data-engine/data-engine-api.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask, jsonify  # From module flask import class Flask
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+
+@app.route('/', methods=['GET'])   # URL '/' to be handled by main() route handler
+def main():    
+    return jsonify(
+        {
+            "user" : "admin",
+            "pass" : "admin"
+        })
+
+@app.route('/city', methods=['GET'])   # URL '/' to be handled by main() route handler
+def GetCity():    
+    return jsonify(
+        {
+            "id" : "1",
+            "name" : "Nagpur"
+        },
+          {
+            "id" : "2",
+            "name" : "Mumbai"
+        },
+    )
+
+if __name__ == '__main__':  # Script executed directly?
+    app.run(host ="localhost", port=4000)  # Launch built-in web server and run this Flask webapp
+
+
+# In[ ]:
+
+
+
+
