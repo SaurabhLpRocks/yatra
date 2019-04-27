9f3d5154957d3326784ce7ccec06160dc48ab651
saurabh.palatkar <saurabh.palatkar@globallogic.com>
2019-04-27 11:24:44 +0530

    Dockerized  python data engine api

--- data-engine/data-engine-api.py
+++ data-engine/data-engine-api.py
@@ -3,12 +3,14 @@
 
 # In[ ]:
 
-
 from flask import Flask, jsonify  # From module flask import class Flask
+
+import tensorflow
+
 app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
 @app.route('/', methods=['GET'])   # URL '/' to be handled by main() route handler
-def main():    
+def main():
     return jsonify(
         {
             "user" : "admin",
@@ -16,7 +18,7 @@ def main():
         })
 
 @app.route('/city', methods=['GET'])   # URL '/' to be handled by main() route handler
-def GetCity():    
+def GetCity():
     return jsonify(
         {
             "id" : "1",
@@ -29,11 +31,7 @@ def GetCity():
     )
 
 if __name__ == '__main__':  # Script executed directly?
-    app.run(host ="localhost", port=4000)  # Launch built-in web server and run this Flask webapp
+    app.run(host='0.0.0.0')  # Launch built-in web server and run this Flask webapp
 
 
 # In[ ]:
-
-
-
-
